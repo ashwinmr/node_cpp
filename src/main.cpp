116fbeb0077fd8d0b45eda749a5d7978c5a73885
ashwin <iashwinrao@gmail.com>
2019-11-14 02:52:14 -0800

    add example function

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,25 @@
 #include <napi.h>
 
+// Example function
+std::string hello(){
+  return "Hello World";
+}
+
+// Wrap example function
+Napi::String HelloWrapped(const Napi::CallbackInfo& info) 
+{
+  return Napi::String::New(info.Env(),hello());
+}
+
 Napi::Object InitAll(Napi::Env env, Napi::Object exports) {
+  exports.Set(
+    "hello", Napi::Function::New(env, HelloWrapped)
+  );
+
   return exports;
 }
 
 NODE_API_MODULE(testaddon, InitAll)
+
+
+
